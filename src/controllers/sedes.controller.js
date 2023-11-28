591b1f0043397c23a239bd86714b268d382e8071
steven-055 <royandyvergaraangulo@gmail.com>
2023-11-28 13:08:18 -0500

    Descripción del cambio

--- src/controllers/sedes.controller.js
+++ src/controllers/sedes.controller.js
@@ -1,6 +1,7 @@
+// Importa la conexión a la base de datos
 import { pool } from '../db.js';
 
-
+// Obtener todas las sedes
 export const getSedes = async (req, res) => {
     try {
         const [rows] = await pool.query('SELECT * FROM tb_sedes');
@@ -12,7 +13,7 @@ export const getSedes = async (req, res) => {
     }
 };
 
-
+// Crear una nueva sede
 export const createSede = async (req, res) => {
     const { distrito } = req.body;
 
@@ -25,43 +26,63 @@ export const createSede = async (req, res) => {
     }
 };
 
+// Actualizar una sede
 export const updateSede = async (req, res) => {
     try {
-        const sedeId = req.params.id_Ubi; 
+        const sedeId = req.params.id_Ubi;
         const { distrito } = req.body;
 
-        const [result] = await pool.query(
+        // Actualizar la sede
+        const [resultSede] = await pool.query(
             'UPDATE tb_sedes SET distrito = IFNULL(?, distrito) WHERE id_Ubi = ?',
             [distrito, sedeId]
         );
 
-        if (result.affectedRows === 0) {
+        if (resultSede.affectedRows === 0) {
             return res.status(404).json({ message: 'Sede no encontrada' });
         }
 
-        res.json({ message: 'Sede actualizada correctamente' });
+        // Actualizar los precios en tb_habitacion_sede según el tipo de habitación
+        const [resultUpdatePrecios] = await pool.query(`
+            UPDATE tb_habitacion_sede
+            SET precio = 
+                CASE tipo
+                    WHEN 'simple' THEN 60
+                    WHEN 'business' THEN 340
+                    WHEN 'presidencial' THEN 850
+                    ELSE precio
+                END
+            WHERE id_Ubi = ?;
+        `, [sedeId]);
+
+        res.json({ message: 'Sede y precios actualizados correctamente' });
     } catch (error) {
+        console.error(error);
         return res.status(500).json({
             message: 'ALGO SALIÓ MAL'
         });
     }
 };
 
-
-
-
+// Eliminar una sede
 export const deleteSede = async (req, res) => {
     try {
-        const sedeId = req.params.id_Ubi; 
-        const [result] = await pool.query('DELETE FROM tb_sedes WHERE id_Ubi = ?', [sedeId]);
+        const sedeId = req.params.id_Ubi;
+        
+        // Eliminar la sede
+        const [resultSede] = await pool.query('DELETE FROM tb_sedes WHERE id_Ubi = ?', [sedeId]);
 
-        if (result.affectedRows <= 0) {
+        if (resultSede.affectedRows <= 0) {
             return res.status(404).json({
                 message: 'Sede no eliminada'
             });
         }
+
+        // No necesitas eliminar los precios en tb_habitacion_sede ya que se eliminarán automáticamente con la restricción FOREIGN KEY.
+
         res.sendStatus(204);
     } catch (error) {
+        console.error(error);
         return res.status(500).json({
             message: 'ALGO SALIÓ MAL'
         });
